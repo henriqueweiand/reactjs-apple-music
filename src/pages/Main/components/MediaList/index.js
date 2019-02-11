5de399b131f355c6ddb060940c52437492b382ca
Henrique Weiand <henriqueweiand@gmail.com>
2019-02-11 15:14:07 -0200

    update

--- src/pages/Main/components/MediaList/index.js
+++ src/pages/Main/components/MediaList/index.js
@@ -1,8 +1,4 @@
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types';
-
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React from 'react';
 
 import Grid from '~/components/Grid';
 import Album from '~/components/Album';
@@ -14,91 +10,52 @@ import ListMusicStruct from '../ListMusicStruct';
 import { Container, Title, ButtonSpace } from './styles';
 import { Button } from '~/styles/components';
 
-import { Creators as SongActions } from '~/store/ducks/song';
-import { Creators as AlbumActions } from '~/store/ducks/album';
-
-class MediaList extends Component {
-  static propTypes = {
-    getAlbumRequest: PropTypes.func.isRequired,
-  };
-
-  componentWillMount() {
-    const { getAlbumRequest, artist } = this.props;
-
-    getAlbumRequest(artist.artistId);
-  }
-
-  renderList = () => (
-    <Container>
-      <Grid container direction="row" justify="space-between" wrap="wrap">
-        <Grid desktop="30" mobile="100">
-          <Album />
-        </Grid>
-        <Grid desktop="65" mobile="100">
-
-          <Hidden to="mobile">
-            <Title>Lil Wayne: Next Steps</Title>
-            <Button style={ButtonSpace}>
-              Listen on
-              {' '}
-              <b>Apple Store</b>
-            </Button>
-          </Hidden>
-
-          <List
-            component={ListMusicStruct}
-            data={[
-              {
-                cover:
-                  'http://3.bp.blogspot.com/-uW82HnOSESs/U0h3alfuC1I/AAAAAAAAo6Y/B3K1iCLV4uw/s1600/edsheeranx.jpg',
-                title: 'Origin',
-                artist: 'New Orleans, LA',
-                time: '4:20',
-              },
-              {
-                cover:
-                  'http://3.bp.blogspot.com/-uW82HnOSESs/U0h3alfuC1I/AAAAAAAAo6Y/B3K1iCLV4uw/s1600/edsheeranx.jpg',
-                title: 'Genre',
-                artist: 'Hip-Hop/Rap',
-                time: '4:20',
-              },
-              {
-                cover:
-                  'http://3.bp.blogspot.com/-uW82HnOSESs/U0h3alfuC1I/AAAAAAAAo6Y/B3K1iCLV4uw/s1600/edsheeranx.jpg',
-                title: 'Born',
-                artist: 'Set 27, 1982',
-                time: '4:20',
-              },
-            ]}
-          />
-        </Grid>
+const MediaList = () => (
+  <Container>
+    <Grid container direction="row" justify="space-between" wrap="wrap">
+      <Grid desktop="30" mobile="100">
+        <Album />
       </Grid>
-    </Container>
-  )
+      <Grid desktop="65" mobile="100">
 
-  render() {
-    const { album } = this.props;
-    return (
-      <Fragment>
-        {
-          album.loading ? (
-            <div>Carregando</div>
-          ) : this.renderList()
-        }
-      </Fragment>
-    );
-  }
-}
+        <Hidden to="mobile">
+          <Title>Lil Wayner: Next Steps</Title>
+          <Button style={ButtonSpace}>
+            Listen on
+            {' '}
+            <b>Apple Store</b>
+          </Button>
+        </Hidden>
 
-const mapStateToProps = state => ({
-  artist: state.artist.data,
-  album: state.album,
-  song: state.song,
-});
+        <List
+          component={ListMusicStruct}
+          data={[
+            {
+              cover:
+                'http://3.bp.blogspot.com/-uW82HnOSESs/U0h3alfuC1I/AAAAAAAAo6Y/B3K1iCLV4uw/s1600/edsheeranx.jpg',
+              title: 'Origin',
+              artist: 'New Orleans, LA',
+              time: '4:20',
+            },
+            {
+              cover:
+                'http://3.bp.blogspot.com/-uW82HnOSESs/U0h3alfuC1I/AAAAAAAAo6Y/B3K1iCLV4uw/s1600/edsheeranx.jpg',
+              title: 'Genre',
+              artist: 'Hip-Hop/Rap',
+              time: '4:20',
+            },
+            {
+              cover:
+                'http://3.bp.blogspot.com/-uW82HnOSESs/U0h3alfuC1I/AAAAAAAAo6Y/B3K1iCLV4uw/s1600/edsheeranx.jpg',
+              title: 'Born',
+              artist: 'Set 27, 1982',
+              time: '4:20',
+            },
+          ]}
+        />
+      </Grid>
+    </Grid>
+  </Container>
+);
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  ...AlbumActions,
-  ...SongActions,
-}, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(MediaList);
+export default MediaList
